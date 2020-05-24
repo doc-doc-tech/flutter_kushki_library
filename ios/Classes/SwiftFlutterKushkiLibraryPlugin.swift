ee1d03d6cc14a97d6b0f29f834fae4f3682cb592
Sergio Rojas <sart1991@gmail.com>
2020-05-23 19:02:18 -0500

    base ios code

--- ios/Classes/SwiftFlutterKushkiLibraryPlugin.swift
+++ ios/Classes/SwiftFlutterKushkiLibraryPlugin.swift
@@ -9,6 +9,36 @@ public class SwiftFlutterKushkiLibraryPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if call.method == "getPlatformVersion" {
+        let platformVersion = UIDevice.current.systemVersion
+        result("iOS " + platformVersion)
+    } else if call.method == "requestSubscriptionToken" {
+        guard let args = call.arguments as? [String: Any] else {
+            let response: [String: Any] = ["code": "ERROR", "message": "There are no arguments"]
+            result(response)
+            return
+        }
+        let response: [String: Any] = ["code": "ERROR", "message": "Unexpected error"]
+        let publicMerchantId = args["publicMerchantId"] as! String
+        let name = args["name"] as! String
+        let number = args["number"] as! String
+        let cvv = args["cvv"] as! String
+        let expiryMonth = args["expiryMonth"] as! String
+        let expiryYear = args["expiryYear"] as! String
+        let currency = args["currency"] as! String
+    }
   }
+    
+    private func initKushki(privateKey: String, publicKey: String, env: String) {
+        
+    }
+    
+    private func requestSubscriptionToken() {
+        
+    }
+    
+    private func getKushkiEnv(env: String) {
+        
+    }
+    
 }
